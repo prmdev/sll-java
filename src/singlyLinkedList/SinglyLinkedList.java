9a69be453f358e04f0d232d31901b61c7cd1cbda
prmdev <93345906+prmdev@users.noreply.github.com>
2020-06-02 16:14:07 -0700

    first commit

---
+++ src/singlyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,48 @@
+package singlyLinkedList;
+
+public class SinglyLinkedList {
+	public Node head;
+    public SinglyLinkedList() {
+        this.head=null;
+    }
+    // SLL methods go here. As a starter, we will show you how to add a node to the list.
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if(head == null) {
+            head = newNode;
+        } else {
+            Node runner = head;
+            while(runner.next != null) {
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+    }
+    
+    public void remove(){
+        if(head == null) {
+            this.head=null;
+        } else {
+            Node runner = head;
+            while(runner.next.next != null) {
+                runner = runner.next;
+            }
+            runner.next = null;
+        }
+    	
+    }
+    
+    public void printValues() {
+    	if(head == null) {
+            System.out.print("List is empty");
+        } else {
+            Node runner = head;
+            while(runner.next != null) {
+            	System.out.print(runner.value);
+                runner = runner.next;
+            }
+        }
+    	
+    }
+
+}
